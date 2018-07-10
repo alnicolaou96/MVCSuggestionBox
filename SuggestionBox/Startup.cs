c10ed189cdb8120f224b89fd225a649559f23d4c
alnicolaou96 <alnicolaou96@gmail.com>
2018-07-10 15:27:28 -0400

    first commit

---
+++ SuggestionBox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuggestionBox.Startup))]
+namespace SuggestionBox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
